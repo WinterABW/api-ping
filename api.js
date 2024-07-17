085a486dd3241f115163a9d3d8926bd51662e01f
WinterABW <skarnanotherblackwinter@gmail.com>
2024-07-17 15:12:11 -0700

    first commit

---
+++ api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const cron = require('node-cron');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get('/ping', (req, res) => {
+    res.send('API 1 is active');
+});
+
+cron.schedule('*/30 * * * * *', async () => {
+    try {
+        const response = await axios.get('http://localhost:3001/ping');
+        console.log(`API fetch response: ${response.data}`);
+    } catch (error) {
+        console.error('Error pinging API fetch: ', error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`API ping listening at http://localhost:${port}`);
+});
